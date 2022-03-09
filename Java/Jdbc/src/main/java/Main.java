cadc030979cbca8b5431d9c94c0676bf2e5ecc05
Luciabrignani <luciabrignani.lb@gmail.com>
2022-03-09 09:14:38 +0100

    09/03/2022

---
+++ Java/Jdbc/src/main/java/Main.java
@@ -0,0 +1,46 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author tss
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        try {
+            String url = "jdbc:mariadb://localhost:3306";
+
+            try(Connection connection = DriverManager.getConnection(url, "tss", "ghiglieno")){
+            System.out.println("Connessione ok...");
+            connection.setCatalog("DBScuola");
+            Statement cmd = connection.createStatement();
+            
+            try ( ResultSet rs = cmd.executeQuery("select * from t_corsi")) {
+                while (rs.next()) {
+                    System.out.println(rs.getInt(1));
+                }
+            }
+                String createTable = "CREATE TABLE `DBScuola`.`new_table` (\n"
+                        + "  `id` INT NOT NULL,\n"
+                        + "  PRIMARY KEY (`id`))";
+                cmd.executeUpdate(createTable);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Errore..." + ex);
+        }
+    }
+
+}
---
+++ Java/com.mycompany_Jdbc_jar_1.0-SNAPSHOT/src/main/java/Main.java
@@ -0,0 +1,47 @@
+
+
+import java.math.BigDecimal;
+import javax.persistence.EntityManager;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author tss
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("dbscuola");       
+        EntityManager em = emf.createEntityManager();
+        
+        em.createQuery("select e from Corso e ", Corso.class)
+                .getResultList()
+                .forEach(System.out::println);
+        
+        Corso tosave = new Corso();
+        tosave.setTitolo("jakarta EE");
+        tosave.setDatainizio(LocalDate.now());
+        tosave.setCosto(BigDecimal.valueOf(120,50));
+        tosave.setCreatoiIl(LocalDateTime.now());
+        
+        em.getTransaction().begin();
+        
+        
+        em.persist(tosave);
+        
+        
+        em.getTransaction().commit();
+    }
+}
